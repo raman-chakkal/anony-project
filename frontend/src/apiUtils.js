2e31c1f70588ed59d51c6c41bd1303590ae55bd1
Ramandeep <ramanchakkal@icloud.com>
2024-11-28 00:14:44 -0500

    commit

--- frontend/src/apiUtils.js
+++
@@ -1,8 +0,0 @@
-export const handleTokenError = (error) => {
-    if (error.response?.status === 401 || error.response?.status === 403) {
-        console.error('Token is invalid or expired. Redirecting to login...');
-        localStorage.removeItem('token');
-        localStorage.removeItem('user');
-        window.location.href = '/login'; // Redirect to the login page
-    }
-};
